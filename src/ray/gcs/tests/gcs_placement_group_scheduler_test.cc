7afe1727b443407ae87b560050e2ad6a7cff2dca
yicheng <yicheng@anyscale.com>
2026-03-24 04:29:03 +0000

    [core] Fix GPU fractional scheduling by tracking per-instance availability
    Signed-off-by: yicheng <yicheng@anyscale.com>

--- src/ray/gcs/tests/gcs_placement_group_scheduler_test.cc
+++ src/ray/gcs/tests/gcs_placement_group_scheduler_test.cc
@@ -145,6 +145,23 @@ class GcsPlacementGroupSchedulerTest : public ::testing::Test {
     gcs_resource_manager_->OnNodeAdd(*node);
   }
 
+  // Simulate a syncer update (available = total) so tests see resources restored.
+  void SimulateSyncerUpdate(const std::shared_ptr<rpc::GcsNodeInfo> &node) {
+    auto node_id = NodeID::FromBinary(node->node_id());
+    syncer::ResourceViewSyncMessage msg;
+    for (const auto &[name, value] : node->resources_total()) {
+      (*msg.mutable_resources_total())[name] = value;
+      rpc::syncer::ResourceInstances instances;
+      auto resource_id = scheduling::ResourceID(name);
+      for (const auto &v :
+           NodeResourceInstanceSet::MakeInstances(resource_id, FixedPoint(value))) {
+        instances.add_values(v.Double());
+      }
+      (*msg.mutable_resources_available_instances())[name] = instances;
+    }
+    gcs_resource_manager_->UpdateFromResourceView(node_id, msg);
+  }
+
   void RemoveNode(const std::shared_ptr<rpc::GcsNodeInfo> &node) {
     rpc::NodeDeathInfo death_info;
     gcs_node_manager_->RemoveNode(
@@ -239,10 +256,10 @@ class GcsPlacementGroupSchedulerTest : public ::testing::Test {
   }
 
   void AddTwoNodes() {
-    auto node0 = GenNodeInfo(0);
-    auto node1 = GenNodeInfo(1);
-    AddNode(node0);
-    AddNode(node1);
+    node0_ = GenNodeInfo(0);
+    node1_ = GenNodeInfo(1);
+    AddNode(node0_);
+    AddNode(node1_);
   }
 
   bool EnsureClusterResourcesAreNotInUse() {
@@ -251,7 +268,8 @@ class GcsPlacementGroupSchedulerTest : public ::testing::Test {
     auto resource_view_before_scheduling = cluster_resource_manager.GetResourceView();
     // Make sure the resources are not used.
     for (const auto &[node_id, node] : resource_view_before_scheduling) {
-      if (node.GetLocalView().total != node.GetLocalView().available) {
+      if (node.GetLocalView().total !=
+          node.GetLocalView().available.ToNodeResourceSet()) {
         return false;
       }
     }
@@ -301,6 +319,8 @@ class GcsPlacementGroupSchedulerTest : public ::testing::Test {
   std::shared_ptr<InMemoryStoreClient> store_client_;
 
   std::vector<std::shared_ptr<rpc::FakeRayletClient>> raylet_clients_;
+  std::shared_ptr<rpc::GcsNodeInfo> node0_;
+  std::shared_ptr<rpc::GcsNodeInfo> node1_;
   std::shared_ptr<GcsResourceManager> gcs_resource_manager_;
   ray::Clock clock_;
   std::shared_ptr<ClusterResourceScheduler> cluster_resource_scheduler_;
@@ -1306,6 +1326,10 @@ TEST_F(GcsPlacementGroupSchedulerTest, TestPrepareFromDeadNodes) {
   GrantPrepareBundleResources(/*grant0=*/{true, Status::OK()},
                               /*grant1=*/{false, Status::IOError("")});
 
+  // Syncer delivers real state after bundle release.
+  SimulateSyncerUpdate(node0_);
+  SimulateSyncerUpdate(node1_);
+
   // Make sure the resources are returned to the cluster_resource_manager at the GCS
   // side.
   ASSERT_TRUE(EnsureClusterResourcesAreNotInUse());
@@ -1333,6 +1357,9 @@ TEST_F(GcsPlacementGroupSchedulerTest, TestPrepareFromNodeWithInsufficientResour
   // node1 grants the schedule request with success=false and status=Status::OK()
   GrantPrepareBundleResources(/*grant0=*/{true, Status::OK()},
                               /*grant1=*/{false, Status::OK()});
+
+  SimulateSyncerUpdate(node0_);
+  SimulateSyncerUpdate(node1_);
 
   // Make sure the resources are returned to the cluster_resource_manager at the GCS
   // side.
@@ -1366,6 +1393,9 @@ TEST_F(GcsPlacementGroupSchedulerTest, TestCommitToDeadNodes) {
   // node0 grants the schedule request status=Status::IOError("")
   // node1 grants the schedule request status=Status::IOError("")
   GrantCommitBundleResources(Status::IOError(""), Status::IOError(""));
+
+  SimulateSyncerUpdate(node0_);
+  SimulateSyncerUpdate(node1_);
 
   // Make sure the resources are returned to the cluster_resource_manager at the GCS
   // side.
